7926d21bc0cde5969e5e53af607b066211f1ac16
Christian Schärf <11158353+schaerfo@users.noreply.github.com>
2025-10-09 23:28:02 +0200

    show transport modes available at each stop

--- ui/src/lib/api/openapi/types.gen.ts
+++ ui/src/lib/api/openapi/types.gen.ts
@@ -16,6 +16,41 @@ export type AlertEffect = 'NO_SERVICE' | 'REDUCED_SERVICE' | 'SIGNIFICANT_DELAYS
 export type AlertSeverityLevel = 'UNKNOWN_SEVERITY' | 'INFO' | 'WARNING' | 'SEVERE';
 
 /**
+ * # Street modes
+ *
+ * - `WALK`
+ * - `BIKE`
+ * - `RENTAL` Experimental. Expect unannounced breaking changes (without version bumps) for all parameters and returned structs.
+ * - `CAR`
+ * - `CAR_PARKING` Experimental. Expect unannounced breaking changes (without version bumps) for all parameters and returned structs.
+ * - `CAR_DROPOFF` Experimental. Expect unannounced breaking changes (without version bumps) for all perameters and returned structs.
+ * - `ODM` on-demand taxis from the Prima+ÖV Project
+ * - `FLEX` flexible transports
+ *
+ * # Transit modes
+ *
+ * - `TRANSIT`: translates to `RAIL,TRAM,BUS,FERRY,AIRPLANE,COACH,CABLE_CAR,FUNICULAR,AREAL_LIFT,OTHER`
+ * - `TRAM`: trams
+ * - `SUBWAY`: subway trains
+ * - `FERRY`: ferries
+ * - `AIRPLANE`: airline flights
+ * - `BUS`: short distance buses (does not include `COACH`)
+ * - `COACH`: long distance buses (does not include `BUS`)
+ * - `RAIL`: translates to `HIGHSPEED_RAIL,LONG_DISTANCE,NIGHT_RAIL,REGIONAL_RAIL,REGIONAL_FAST_RAIL,METRO,SUBWAY`
+ * - `METRO`: metro trains
+ * - `HIGHSPEED_RAIL`: long distance high speed trains (e.g. TGV)
+ * - `LONG_DISTANCE`: long distance inter city trains
+ * - `NIGHT_RAIL`: long distance night trains
+ * - `REGIONAL_FAST_RAIL`: regional express routes that skip low traffic stops to be faster
+ * - `REGIONAL_RAIL`: regional train
+ * - `CABLE_CAR`: Cable tram. Used for street-level rail cars where the cable runs beneath the vehicle (e.g., cable car in San Francisco).
+ * - `FUNICULAR`: Funicular. Any rail system designed for steep inclines.
+ * - `AREAL_LIFT`: Aerial lift, suspended cable car (e.g., gondola lift, aerial tramway). Cable transport where cabins, cars, gondolas or open chairs are suspended by means of one or more cables.
+ *
+ */
+export type Mode = 'WALK' | 'BIKE' | 'RENTAL' | 'CAR' | 'CAR_PARKING' | 'CAR_DROPOFF' | 'ODM' | 'FLEX' | 'TRANSIT' | 'TRAM' | 'SUBWAY' | 'FERRY' | 'AIRPLANE' | 'METRO' | 'BUS' | 'COACH' | 'RAIL' | 'HIGHSPEED_RAIL' | 'LONG_DISTANCE' | 'NIGHT_RAIL' | 'REGIONAL_FAST_RAIL' | 'REGIONAL_RAIL' | 'CABLE_CAR' | 'FUNICULAR' | 'AREAL_LIFT' | 'OTHER';
+
+/**
  * A time interval.
  * The interval is considered active at time t if t is greater than or equal to the start time and less than the end time.
  *
@@ -177,6 +212,12 @@ export type LocationType = 'ADDRESS' | 'PLACE' | 'STOP';
 export type Match = {
     type: LocationType;
     /**
+     * Modes of transport available at the stop.
+     * Only included if `type == STOP`
+     *
+     */
+    modes?: Array<Mode>;
+    /**
      * list of non-overlapping tokens that were matched
      */
     tokens: Array<Token>;
@@ -210,6 +251,10 @@ export type Match = {
      * house number
      */
     houseNumber?: string;
+    /**
+     * ISO3166-1 country code from OpenStreetMap
+     */
+    country?: string;
     /**
      * zip code
      */
@@ -253,41 +298,6 @@ export type PedestrianSpeed = number;
  * Average speed for bike routing in meters per second
  */
 export type CyclingSpeed = number;
-
-/**
- * # Street modes
- *
- * - `WALK`
- * - `BIKE`
- * - `RENTAL` Experimental. Expect unannounced breaking changes (without version bumps) for all parameters and returned structs.
- * - `CAR`
- * - `CAR_PARKING` Experimental. Expect unannounced breaking changes (without version bumps) for all parameters and returned structs.
- * - `CAR_DROPOFF` Experimental. Expect unannounced breaking changes (without version bumps) for all perameters and returned structs.
- * - `ODM` on-demand taxis from the Prima+ÖV Project
- * - `FLEX` flexible transports
- *
- * # Transit modes
- *
- * - `TRANSIT`: translates to `RAIL,TRAM,BUS,FERRY,AIRPLANE,COACH,CABLE_CAR,FUNICULAR,AREAL_LIFT,OTHER`
- * - `TRAM`: trams
- * - `SUBWAY`: subway trains
- * - `FERRY`: ferries
- * - `AIRPLANE`: airline flights
- * - `BUS`: short distance buses (does not include `COACH`)
- * - `COACH`: long distance buses (does not include `BUS`)
- * - `RAIL`: translates to `HIGHSPEED_RAIL,LONG_DISTANCE,NIGHT_RAIL,REGIONAL_RAIL,REGIONAL_FAST_RAIL,METRO,SUBWAY`
- * - `METRO`: metro trains
- * - `HIGHSPEED_RAIL`: long distance high speed trains (e.g. TGV)
- * - `LONG_DISTANCE`: long distance inter city trains
- * - `NIGHT_RAIL`: long distance night trains
- * - `REGIONAL_FAST_RAIL`: regional express routes that skip low traffic stops to be faster
- * - `REGIONAL_RAIL`: regional train
- * - `CABLE_CAR`: Cable tram. Used for street-level rail cars where the cable runs beneath the vehicle (e.g., cable car in San Francisco).
- * - `FUNICULAR`: Funicular. Any rail system designed for steep inclines.
- * - `AREAL_LIFT`: Aerial lift, suspended cable car (e.g., gondola lift, aerial tramway). Cable transport where cabins, cars, gondolas or open chairs are suspended by means of one or more cables.
- *
- */
-export type Mode = 'WALK' | 'BIKE' | 'RENTAL' | 'CAR' | 'CAR_PARKING' | 'CAR_DROPOFF' | 'ODM' | 'FLEX' | 'TRANSIT' | 'TRAM' | 'SUBWAY' | 'FERRY' | 'AIRPLANE' | 'METRO' | 'BUS' | 'COACH' | 'RAIL' | 'HIGHSPEED_RAIL' | 'LONG_DISTANCE' | 'NIGHT_RAIL' | 'REGIONAL_FAST_RAIL' | 'REGIONAL_RAIL' | 'CABLE_CAR' | 'FUNICULAR' | 'AREAL_LIFT' | 'OTHER';
 
 /**
  * - `NORMAL` - latitude / longitude coordinate or address
