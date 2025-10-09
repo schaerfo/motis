5fb972972912e62ff81c965144e607439289040f
Christian Schärf <11158353+schaerfo@users.noreply.github.com>
2025-10-09 23:27:47 +0200

    return transport modes available at each stop

--- include/motis/timetable/clasz_to_mode.h
+++ include/motis/timetable/clasz_to_mode.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "nigiri/routing/clasz_mask.h"
 #include "nigiri/types.h"
 
 #include "motis-api/motis-api.h"
@@ -7,5 +8,6 @@
 namespace motis {
 
 api::ModeEnum to_mode(nigiri::clasz);
+std::vector<api::ModeEnum> clasz_mask_to_modes(nigiri::routing::clasz_mask_t);
 
 }  // namespace motis
